0db640eb96e495294e8df4911fee4bc9c8054c8c
Sebastian Lundström <khueue@gmail.com>
2012-11-06 06:46:04 +0100

    Refactoring and cleanup

--- src/json.pl
+++ src/json.pl
@@ -23,18 +23,18 @@ doc_json(Doc, Json) :-
     phrase(parse_object(Doc), JsonChars, _JsonCharsRest).
 
 parse_object(Doc) -->
-    white,
+    ws,
     ['{'],
     parse_members(Doc),
     !,
     ['}'],
-    white.
-parse_object(Doc) -->
-    white,
+    ws.
+parse_object([]) -->
+    ws,
     ['{'],
-    white,
+    ws,
     ['}'],
-    white.
+    ws.
 
 parse_members([Key-Value|Pairs]) -->
     parse_pair(Key-Value),
@@ -45,13 +45,13 @@ parse_members([Key-Value]) -->
     parse_pair(Key-Value).
 
 parse_pair(Key-Value) -->
-    white,
+    ws,
     parse_key(Key),
-    white,
+    ws,
     [':'],
-    white,
+    ws,
     parse_value(Value),
-    white.
+    ws.
 
 parse_key(Key) -->
     ['"'],
@@ -67,34 +67,52 @@ parse_value(Value) -->
     parse_integer(Value).
 
 parse_integer(Integer) -->
-    parse_integer_aux(Chars),
-    { core:atom_chars(Atom, Chars) },
-    { core:atom_number(Atom, Integer) }.
-
-parse_integer_aux([Char|Chars]) -->
-    [Char],
-    { core:char_type(Char, digit) },
+    parse_digit_nonzero(FirstDigit),
     !,
-    parse_integer_aux(Chars).
-parse_integer_aux([]) --> [].
+    parse_digits(Digits),
+    { core:atom_chars(Atom, [FirstDigit|Digits]) },
+    { core:atom_number(Atom, Integer) }.
+parse_integer(Integer) -->
+    parse_digit(Digit),
+    { core:atom_number(Digit, Integer) }.
+
+parse_digit_nonzero(Digit) -->
+    parse_digit(Digit),
+    { Digit \== '0' }.
+
+parse_digits([Digit|Digits]) -->
+    parse_digit(Digit),
+    !,
+    parse_digits(Digits).
+parse_digits([]) --> [].
+
+parse_digit(Digit) -->
+    [Digit],
+    { core:char_type(Digit, digit) }.
 
 parse_chars(Atom) -->
     parse_chars_aux(Chars),
     { core:atom_chars(Atom, Chars) }.
 
 parse_chars_aux([Char|Chars]) -->
-    [Char],
-    { valid_char(Char) },
+    parse_char(Char),
     !,
     parse_chars_aux(Chars).
 parse_chars_aux([]) --> [].
 
+parse_char(Char) -->
+    [Char],
+    { valid_char(Char) }.
+
 valid_char(Char) :-
     \+ lists:memberchk(Char, ['"']).
 
-white -->
-    [Char],
-    { core:char_type(Char, space) },
+ws -->
+    ws_char,
     !,
-    white.
-white --> [].
+    ws.
+ws --> [].
+
+ws_char -->
+    [Char],
+    { core:char_type(Char, space) }.
